fea854505d988a1b7a053b9df3dd3c80d76a28b9
Alan <alantessier97@gmail.com>
2021-06-01 18:26:10 -0400

    img fields added

--- base/main.py
+++ base/main.py
@@ -3,4 +3,7 @@ from env.env import Env
 if __name__ == '__main__':
     csv = "/media/alan/seagate/Downloads/BTCUSDT_Binance_futures_data_hour (1).csv"
     env = Env(csv)
-    env.plot_animation()
+    s = env.reset()
+    # env.display_grammian()
+    env.show()
+    print(s.shape)
