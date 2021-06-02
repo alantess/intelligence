b358925dfdbb2ff63c9a881d16865e2413174c0c
Alan <alantessier97@gmail.com>
2021-06-02 16:00:43 -0400

    env finished

--- base/main.py
+++ base/main.py
@@ -3,8 +3,13 @@ from env.env import Env
 if __name__ == '__main__':
     csv = "/media/alan/seagate/Downloads/Binance_LTCUSDT_minute_ds.csv"
     env = Env(csv)
-    s = env.reset()
 
-    # env.display_grammian()
-    # env.show()
-    # print(s.shape)
+    for i in range(2):
+        print("Executing")
+        done = False
+        s = env.reset()
+        while not done:
+            a = 0
+            s_, r, d, _ = env.step(a)
+            env.display_grammian()
+            s = s_
